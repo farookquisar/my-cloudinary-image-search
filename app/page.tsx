eee71a889bd06a28f37669b298af22cbb119b388
farookquisar <farook.quisar@gmail.com>
2024-01-08 15:04:02 +0300

    1

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,8 @@ export default async function Home({ searchParams }: { searchParams: any }) {
 
   const { resources } = await cloudinary.search.expression(expression).execute();
 
+
+
   async function search(data: FormData) {
     'use server';
     redirect(`/?query=${data.get('query')}`);
@@ -37,7 +39,7 @@ export default async function Home({ searchParams }: { searchParams: any }) {
           <input
             name="query"
             type="search"
-            className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent  px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
             placeholder="Search"
             aria-label="Search"
             aria-describedby="button-addon2" />
@@ -50,9 +52,9 @@ export default async function Home({ searchParams }: { searchParams: any }) {
               fill="currentColor"
               className="h-5 w-5">
               <path
-                fill-rule="evenodd"
+                
                 d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                clip-rule="evenodd" />
+                 />
             </svg>
           </span>
         </form>
